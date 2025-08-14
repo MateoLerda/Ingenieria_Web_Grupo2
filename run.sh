2ad7d31becf50a57809422d5c52299ae5ac88c6c
Mijael Segura <seguramijael03@gmail.com>
2025-08-13 22:49:46 -0300

    deleting whitespaces

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -o errexit
 
 # run the web app server
 cd $(dirname $(find . | grep manage.py$))
-uv run gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application      
+uv run gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
