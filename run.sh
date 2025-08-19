82bd47cd8550f64deca5d769d45e4a572c23d43c
Tu Nombre <mateolerda03@gmail.com>
2025-08-19 17:09:57 -0300

    fix run.sh error

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@ set -o errexit
 
 # run the web app server
 cd $(dirname $(find . | grep manage.py$))
-uv run gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
+#uv run gunicorn $(dirname $(find . | grep wsgi.py$) | sed "s/\.\///g").wsgi:application
+uv run gunicorn PartyFinder.wsgi:application
