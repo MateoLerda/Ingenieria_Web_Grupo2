fee8a61ddbfb928ef277fdf055dd63b4a28942cb
Mijael Segura <seguramijael03@gmail.com>
2025-08-29 09:27:59 -0300

    removed bootswatch

--- mainapp/views.py
+++ mainapp/views.py
@@ -81,4 +81,3 @@ def logout_view(request):
 def event_detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     return render(request, 'event_detail.html', {'event': event})
-
