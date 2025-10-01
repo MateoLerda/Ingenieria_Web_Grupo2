3b005d07d3476d200602f31be0ab2ebf5e6dd9aa
Tu Nombre <mateolerda03@gmail.com>
2025-09-30 21:42:21 -0300

    modificacion archivos estaticos

--- PartyFinder/settings.py
+++ PartyFinder/settings.py
@@ -163,12 +163,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Solo para desarrollo
-if DEBUG:
-    STATICFILES_DIRS = [
+STATICFILES_DIRS = [
         BASE_DIR / 'static',
     ]
-else:
-    STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
