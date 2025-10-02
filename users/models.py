9fb75c06bbca0d383003f14cf258b0b55655347c
Mijael Segura <seguramijael03@gmail.com>
2025-10-01 21:07:13 -0300

    fix sign in with social apps

--- users/models.py
+++ users/models.py
@@ -7,12 +7,12 @@ class CustomUserModel(AbstractUser):
     full_name = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, unique=True)
-    birth_date = models.DateField()
+    birth_date = models.DateField(null=True, blank=True)
     first_name = None
     last_name = None
 
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email', 'full_name', 'birth_date']
+    REQUIRED_FIELDS = ['email', 'full_name']
 
     class Meta:
         ordering = ['date_joined']
